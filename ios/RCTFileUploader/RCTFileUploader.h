0a6e180b72b316fc309d22b7d5e46c53e480a40a
Paul Kane <pk2004@gmail.com>
2018-03-29 11:12:18 +0100

    Support new import style.

--- ios/RCTFileUploader/RCTFileUploader.h
+++ ios/RCTFileUploader/RCTFileUploader.h
@@ -1,7 +1,7 @@
 //
 // Copyright © 2016 Fabrice Armisen <farmisen@gmail.com>
 // This program is free software. It comes without any warranty, to
-// the extent permitted by applicable law. You can redistribute it and/or modify 
+// the extent permitted by applicable law. You can redistribute it and/or modify
 // it under the terms of the Do What The Fuck You Want To Public License, Version 2,
 // as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
 //
@@ -9,11 +9,12 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "RCTBridgeModule.h"
+#if __has_include(<React/RCTBridgeModule.h>)
+  #import <React/RCTBridgeModule.h>
+#else
+  #import "RCTBridgeModule.h"
+#endif
 
 @interface RCTFileUploader : NSObject<RCTBridgeModule>
 
 @end
-
-
-
